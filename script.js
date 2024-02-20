0bfa5d8802946fa651f64bbf5fa7ab98261452a3
felipedorea <122095865+felipedorea@users.noreply.github.com>
2024-02-20 09:49:33 -0300

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = document.getElementById("nome").value;
+let email = document.getElementById("email").value;
+let onibus = document.getElementById("onibus").value;
+let mapa = document.getElementById("mapa").value;
+let message = document.getElementById("message").value;
+
+
+function mostrarMensagem(){
+    
+}
+
